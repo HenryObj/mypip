e77a527ea475643ba3fe099c677d00419bd60142
HenryObj <hobegi@gmail.com>
2024-03-29 08:41:26 +0200

    Print colors

--- henryobj/base.py
+++ henryobj/base.py
@@ -238,6 +238,32 @@ def log_warning(warning:str, func: Callable[..., Any], additional_info: str = ""
     ----------------------------------------------------------------
     """)
 
+def print_color(message, color="blue"):
+    """
+    To print a message in a specific color.
+    Check the function for the list of available colors.
+    """
+    colors = {
+        "black": "\033[30m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "light_gray": "\033[37m",
+        "dark_gray": "\033[90m",
+        "light_red": "\033[91m",
+        "light_green": "\033[92m",
+        "light_yellow": "\033[93m",
+        "light_blue": "\033[94m",
+        "light_magenta": "\033[95m",
+        "light_cyan": "\033[96m",
+        "white": "\033[97m",
+        "end": "\033[0m",  # Resets the color/style
+    }
+    print(f"{colors.get(color, colors['white'])}{message}{colors['end']}")
+
 # local tests
 def lprint(*args: Any):
     '''
